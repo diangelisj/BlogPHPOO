16a6c6576d33814356ab5be5836ddf4e58b0d8ac
diangelisj <diangelisj@outlook.com>
2013-11-15 02:05:40 -0200

    Envio com login completo e sessao

--- admin/Index.php
+++ admin/Index.php
@@ -1,2 +1,45 @@
-
 <?php
+
+session_start();
+
+require_once 'class/Acess.php';
+
+if(isset($_POST['logar'])):
+    $Acess = new Acess();
+    $Acess->setUser($_POST['usuario']);
+    $Acess->setPass($_POST['senha']);
+    $Acess->login();
+    if($Acess->getError()):
+      echo  $err=$Acess->getError();
+    endif;
+endif;
+;?>
+<!DOCTYPE HTML PUBLIC "-//W3C/DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html"; charset="utf-8"/>
+        <title>Blog PHPOO </title>
+        <link href="style_acess.css" rel="stylesheet"type="text/css"/>
+    </head>
+       
+    <body>
+        <div id="login">
+            <?php echo $_SESSION['nome_logado'] ;?>
+            <form action ="" method="POST">
+                <label for ="usuario">Usuário:</label> 
+                <input type="text"  name="usuario" class="input"/>
+                
+                <label for="senha"> Senha</label>
+                <input type="text" name="senha"class="input"/>
+                
+                <label for="submit"></label>
+                <input type="submit" name="logar" value="ok" clas="input_submit"/>
+                      
+                
+            </form>
+        </div>
+        
+    </body>
+    
+    
+</html>
--- admin/class/Acess.php
+++ admin/class/Acess.php
@@ -13,9 +13,58 @@ class Acess  extends Conn implements iAcess{
     private $pass;
     private $error;
     
+    public function getUser() {
+        return $this->user;
+    }
+
+    public function getPass() {
+        return $this->pass;
+    }
+
+    public function getError() {
+        return $this->error;
+    }
+
+    public function setUser($user) {
+        $this->user = $user;
+    }
+
+    public function setPass($pass) {
+        $this->pass = $pass;
+    }
+
+    public function setError($error) {
+        $this->error = $error;
+    }
+
+        
     public function login(){
+        $pdo = parent::getDB();
         
+        try{
+        $login =$pdo->prepare("SELECT * FROM admin WHERE admin_usuario = :usuario
+            AND admin_senha = :senha");
+        $login->bindValue(":usuario",$this->getUser());
+        $login->bindValue(":senha",$this->getPass());
         
+        $login->execute();
+        
+        if($login->rowCount()==1):
+            $data = $login->fetch(PDO::FETCH_ASSOC);
+        $_SESSION['logado']=true;
+        $_SESSION['nome_logado']=$data['admin_nome'];
+            
+        return true;
+        
+            else:
+                
+                $this->setError("Usuário ou senha inválido!");
+            $_SESSION['nome_logado']="Deslogado!";
+        endif;
+        }  catch (PDOException $e){
+            
+            echo "Aconteceu um erro : ".$e->getMessage();
+        }
     }
 
     public function logout() {
